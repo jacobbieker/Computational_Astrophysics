f118ef2c3f95c31bc23b153e0a012aafb36f31aa
Jacob Bieker <jacob@bieker.tech>
2019-02-21 13:35:39 +0100

    Sorta messed up version one

--- assignment_two/GravStellar.py
+++ assignment_two/GravStellar.py
@@ -39,13 +39,25 @@ class GravStellar(object):
         self.stellar_mass_loss_time_step_fraction = stellar_mass_loss_frac
         self.stellar_start_time = stellar_start_time
 
+        self.gravity = None
+        self.stellar = None
 
-    def advance_stellar(self):
-        return NotImplementedError
+        self.time = 0.0 | end_time.unit
 
-    def advance_gravity(self):
-        return NotImplementedError
 
+    def advance_stellar(self, ts, dt):
+        E0 = self.gravity.kinetic_energy + self.gravity.potential_energy
+        ts += dt
+        self.stellar.evolve_model(ts)
+        channel_from_stellar.copy_attributes(["mass"])
+        channel_from_framework_to_gd.copy_attributes(["mass"])
+        return ts, gravity.kinetic_energy + gravity.potential_energy - E0
+
+    def advance_gravity(self, tg, dt):
+        tg += dt
+        self.gravity.evolve_model(tg)
+        channel_from_gd_to_framework.copy()
+        return tg
     def get_orbital_period(self):
         return 2*np.pi*(a**3/(constants.G*Mtot)).sqrt()
 
@@ -69,7 +81,79 @@ class GravStellar(object):
         return ain, ein, aout, eout
 
     def evolve(self):
-        return NotImplementedError
+
+        while self.time < self.end_time:
+
+            # TODO: Add determining timestep
+
+            if self.integration_scheme == 1:
+
+                ts, dE_se = self.advance_stellar(ts, dt)
+                time = self.advance_gravity(time, dt)
+
+            elif self.integration_scheme == 2:
+
+                time = self.advance_gravity(time, dt)
+                ts, dE_se = self.advance_stellar(ts, dt)
+
+            else:
+
+                dE_se = zero
+                #ts, dE_se = advance_stellar(ts, dt/2)
+                time = self.advance_gravity(time, dt)
+                #ts, dE = advance_stellar(ts, dt/2)
+                #dE_se += dE
+
+            if self.time >= self.t_diag:
+
+                t_diag = time + dt_diag
+
+                Ekin = gravity.kinetic_energy
+                Epot = gravity.potential_energy
+                Etot = Ekin + Epot
+                dE = Etot_prev - Etot
+                Mtot = triple.mass.sum()
+                print("T=", time, end=' ')
+                print("M=", Mtot, "(dM[SE]=", Mtot / Mtriple, ")", end=' ')
+                print("E= ", Etot, "Q= ", Ekin / Epot, end=' ')
+                print("dE=", (Etot_init - Etot) / Etot, "ddE=", (Etot_prev - Etot) / Etot, end=' ')
+                print("(dE[SE]=", dE_se / Etot, ")")
+                Etot_init -= dE
+                Etot_prev = Etot
+                ain, ein, aout, eout = get_orbital_elements_of_triple(triple)
+                print("Triple elements t=", (4 | units.Myr) + time,
+                      "inner:", triple[0].mass, triple[1].mass, ain, ein,
+                      "outer:", triple[2].mass, aout, eout)
+
+                t.append(time.value_in(units.Myr))
+                smai.append(ain/ain_0)
+                ecci.append(ein/ein_0)
+                smao.append(aout/aout_0)
+                ecco.append(eout/eout_0)
+
+                if eout > 1.0 or aout <= zero:
+                    print("Binary ionized or merged")
+                    break
+
+        self.gravity.stop()
+        self.stellar.stop()
+
+        return t, smai, ecci, smao, ecco
+
+    def set_gravity(self, stars):
+        converter = nbody_system.nbody_to_si(stars.mass.sum(), aout_0)
+        self.gravity = self.gravity_model(converter)
+        self.gravity.particles.add_particles(stars)
+
+        channel_from_framework_to_gd = stars.new_channel_to(self.gravity.particles)
+        channel_from_gd_to_framework = self.gravity.particles.new_channel_to(stars)
+
+        total_initial_energy = self.gravity.kinetic_energy + self.gravity.potential_energy
+        total_previous_energy = total_initial_energy
+
+        self.gravity.particles.move_to_center()
+
+
 
     def age_stars(self):
         mult_stars = Particles(self.num_bodies)
